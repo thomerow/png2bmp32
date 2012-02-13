8f6774f8df8d3d5f1ba54156b9fbe5c806507512
Thorsten Kummerow <thorsten.kummerow@googlemail.com>
2012-02-13 13:08:53 +0100

    Minor cosmetics.

--- ImageTools.cs
+++ ImageTools.cs
@@ -9,9 +9,9 @@ namespace png2bmp32
    {
       internal static bool IsPNG(byte[] imageData)
       {
-         const UInt32 nPNGHead = 0x474e5089;
+         const UInt32 uPNGHead = 0x474e5089;
 
-         return ((imageData.Length >= 4) && (BitConverter.ToUInt32(imageData, 0) == nPNGHead));
+         return ((imageData.Length >= 4) && (BitConverter.ToUInt32(imageData, 0) == uPNGHead));
       }
    }
 }

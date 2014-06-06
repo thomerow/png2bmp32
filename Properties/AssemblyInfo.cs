8a49ccc37e7f45bdf295a30051794a4485f300ac
Thorsten Kummerow <thorsten@textcontrol.com>
2014-06-06 10:39:51 +0200

    Removed test project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,30 +8,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("png2bmp32")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Thorsten Kummerow")]
 [assembly: AssemblyProduct("png2bmp32")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Thorsten Kummerow")]
+[assembly: AssemblyTrademark("png2bmp32")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3af21fa0-265c-47b7-a506-21a25df8209e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
